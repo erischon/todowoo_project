bfce5b7aad879af302c0f408925026e5742e78ae
Eri Schon <erischon@gmail.com>
2021-02-03 11:03:47 +0100

    Update urls.py

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
 
     #auth
     path('signup/', views.signupuser, name="signupuser"),
+    path('login/', views.loginuser, name="loginuser"),
     path('logout/', views.logoutuser, name="logoutuser"),
 
     #todos
