34fcd69603444de4c7677c3a1c06ac0fb6a547ed
Eri Schon <erischon@gmail.com>
2021-02-11 14:11:05 +0100

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -62,5 +62,13 @@ def currenttodos(request):
 
 def viewtodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk)
-    form = TodoForm(instance=todo)
-    return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form})
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form, 'error':'Bad info.'})
