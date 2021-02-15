8469e48d0680ea74309fb490bec6412d70d15392
Eri Schon <erischon@gmail.com>
2021-02-15 10:49:09 +0100

    Update urls.py

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -30,5 +30,6 @@ urlpatterns = [
     path('create/', views.createtodo, name="createtodo"),    
     path('current/', views.currenttodos, name="currenttodos"),
     path('todo/<int:todo_pk>', views.viewtodo, name="viewtodo"),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name="completetodo"),
 
 ]
